e6a66d95b083c3bb0a9d9ed7bc4b1921e0915d8f
Bruno Viotto <brunoviottosantos@gmail.com>
2023-05-20 16:45:57 -0300

    Removing the public keywords from the IQueryableRepository methds interface since they are public by default. #5

--- Viotto.DomainDrivenDesign.Repository/IQueryableRepository.cs
+++ Viotto.DomainDrivenDesign.Repository/IQueryableRepository.cs
@@ -6,14 +6,14 @@ namespace Viotto.DomainDrivenDesign.Repository;
 public interface IQueryableRepository<TModel, TId>
     where TModel : IEntity<TId>
 {
-    public IQueryable<TModel> GetAll();
-    public IQueryable<TModel> GetAllNoTracking();
+    IQueryable<TModel> GetAll();
+    IQueryable<TModel> GetAllNoTracking();
 
-    public TModel GetById(TId id);
-    public TModel GetByIdNoTracking(TId id);
-    public Task<TModel> GetByIdAsync(TId id);
-    public Task<TModel> GetByIdNoTrackingAsync(TId id);
+    TModel GetById(TId id);
+    TModel GetByIdNoTracking(TId id);
+    Task<TModel> GetByIdAsync(TId id);
+    Task<TModel> GetByIdNoTrackingAsync(TId id);
 
-    public IQueryable<TModel> GetByIds(IEnumerable<TId> ids);
-    public IQueryable<TModel> GetByIdsNoTracking(IEnumerable<TId> ids);
+    IQueryable<TModel> GetByIds(IEnumerable<TId> ids);
+    IQueryable<TModel> GetByIdsNoTracking(IEnumerable<TId> ids);
 }
