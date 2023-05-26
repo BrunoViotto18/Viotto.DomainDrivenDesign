481713959a0b04b2ec3bea82adcaee3882cdd1e0
Santos Bruno (CtP/ETS) <brunoviottosantos@gmail.com>
2023-05-26 16:15:11 -0300

    Adding tests for the Create methods of the BaseRepository abstract class base implementations. #11

---
+++ Viotto.DomainDrivenDesign.Repository.UnitTests/BaseRepository.Creatable.Tests.cs
@@ -0,0 +1,131 @@
+﻿using FluentAssertions;
+
+namespace Viotto.DomainDrivenDesign.Repository.UnitTests;
+
+
+public partial class BaseRepositoryTests
+{
+    //! CREATE
+
+    [Fact]
+    public void Create_ShouldCreateAndSaveTheObjectInTheDatabase_WhenTheObjectPrimaryKeyIsNotSet()
+    {
+        // Arrange
+        var obj = _testGenerator.Generate(1).First();
+
+        // Act
+        _sut.Create(obj);
+        _context.ChangeTracker.Clear();
+
+        // Assert
+        _context.Test.Count().Should().Be(1);
+        _context.Test.First().Should().Be(obj);
+    }
+
+    [Fact]
+    public void Create_ShouldUpdateTheOriginalObjectId_WhenTheObjectIsSuccesfullySavedToTheDatabase()
+    {
+        // Arrange
+        var obj = _testGenerator.Generate(1).First();
+
+        // Act
+        _sut.Create(obj);
+        _context.ChangeTracker.Clear();
+
+        // Assert
+        _context.Test.First().Should().Be(obj);
+    }
+
+    //! CREATE ASYNC
+
+    [Fact]
+    public async Task CreateAsync_ShouldCreateAndSaveTheObjectInTheDatabase_WhenTheObjectPrimaryKeyIsNotSet()
+    {
+        // Arrange
+        var obj = _testGenerator.Generate(1).First();
+
+        // Act
+        await _sut.CreateAsync(obj);
+        _context.ChangeTracker.Clear();
+
+        // Assert
+        _context.Test.Count().Should().Be(1);
+        _context.Test.First().Should().Be(obj);
+    }
+
+    [Fact]
+    public async Task CreateAsync_ShouldUpdateTheOriginalObjectId_WhenTheObjectIsSuccesfullySavedToTheDatabase()
+    {
+        // Arrange
+        var obj = _testGenerator.Generate(1).First();
+
+        // Act
+        await _sut.CreateAsync(obj);
+        _context.ChangeTracker.Clear();
+
+        // Assert
+        _context.Test.First().Should().Be(obj);
+    }
+
+    //! CREATE RANGE
+
+    [Fact]
+    public void CreateRange_ShouldCreateAndSaveMultipleObjectsInTheDatabase_WhenEveryObjectPrimaryKeyIsNotSet()
+    {
+        // Arrange
+        var data = _testGenerator.Generate(10);
+
+        // Act
+        _sut.CreateRange(data);
+        _context.ChangeTracker.Clear();
+
+        // Assert
+        _context.Test.Count().Should().Be(10);
+        _context.Test.Should().BeEquivalentTo(data);
+    }
+
+    [Fact]
+    public void CreateRange_ShouldUpdateTheOriginalObjectsId_WhenTheObjectsAreSavedSuccessfully()
+    {
+        // Arrange
+        var data = _testGenerator.Generate(10);
+
+        // Act
+        _sut.CreateRange(data);
+        _context.ChangeTracker.Clear();
+
+        // Assert
+        _context.Test.Should().BeEquivalentTo(data);
+    }
+
+    //! CREATE RANGE ASYNC
+
+    [Fact]
+    public async Task CreateRangeAsync_ShouldCreateAndSaveMultipleObjectsInTheDatabase_WhenEveryObjectPrimaryKeyIsNotSet()
+    {
+        // Arrange
+        var data = _testGenerator.Generate(10);
+
+        // Act
+        await _sut.CreateRangeAsync(data);
+        _context.ChangeTracker.Clear();
+
+        // Assert
+        _context.Test.Count().Should().Be(10);
+        _context.Test.Should().BeEquivalentTo(data);
+    }
+
+    [Fact]
+    public async Task CreateRangeAsync_ShouldUpdateTheOriginalObjectsId_WhenTheObjectsAreSavedSuccessfully()
+    {
+        // Arrange
+        var data = _testGenerator.Generate(10);
+
+        // Act
+        await _sut.CreateRangeAsync(data);
+        _context.ChangeTracker.Clear();
+
+        // Assert
+        _context.Test.Should().BeEquivalentTo(data);
+    }
+}
--- Viotto.DomainDrivenDesign.Repository.UnitTests/Models/Test.cs
+++ Viotto.DomainDrivenDesign.Repository.UnitTests/Models/Test.cs
@@ -9,4 +9,13 @@ public class Test : IEntity<long>
     public string Name { get; set; }
     public DateOnly DateOfBirth { get; set; }
     public int LuckyNumber { get; set; }
+
+
+    public override bool Equals(object obj)
+    {
+        if (obj is not Test test)
+            return false;
+
+        return test.Id == Id && test.Name == Name && test.DateOfBirth == DateOfBirth && test.LuckyNumber == LuckyNumber;
+    }
 }
