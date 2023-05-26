0913bf4b78cf6ae6350abd0f2b9dc0b968c8cd57
Santos Bruno (CtP/ETS) <brunoviottosantos@gmail.com>
2023-05-26 13:30:06 -0300

    Adding tests for the transaction methods base implementation of the BaseRepository abstract class. #11

--- Viotto.DomainDrivenDesign.Repository.UnitTests/BaseRepository.Tests.cs
+++ Viotto.DomainDrivenDesign.Repository.UnitTests/BaseRepository.Tests.cs
@@ -4,6 +4,7 @@ namespace Viotto.DomainDrivenDesign.Repository.UnitTests;
 
 using Contexts;
 using Fixtures;
+using FluentAssertions;
 using Models;
 using Repositories;
 
@@ -24,6 +25,51 @@ public partial class BaseRepositoryTests : IAsyncLifetime
         _respawn = testSetup.RespawnDatabase;
 
         _sut = new TestRepository(_context);
+    }
+
+
+    [Fact]
+    public void BeginTransaction_ShouldReturnAValidRepositoryTransaction_Always()
+    {
+        // Arrange
+        var data1 = _testGenerator.Generate(10);
+        var data2 = _testGenerator.Generate(20).Skip(10);
+
+        // Act
+        var transaction1 = _sut.BeginTransaction();
+        _context.AddRange(data1);
+        _context.SaveChanges();
+        transaction1.Commit();
+
+        var transaction2 = _sut.BeginTransaction();
+        _context.AddRange(data2);
+        _context.SaveChanges();
+        transaction2.Rollback();
+
+        // Assert
+        _context.Test.Should().BeEquivalentTo(data1);
+    }
+
+    [Fact]
+    public async Task BeginTransactionAsync_ShouldReturnAValidRepositoryTransaction_Always()
+    {
+        // Arrange
+        var data1 = _testGenerator.Generate(10);
+        var data2 = _testGenerator.Generate(20).Skip(10);
+
+        // Act
+        var transaction1 = await _sut.BeginTransactionAsync();
+        _context.AddRange(data1);
+        await _context.SaveChangesAsync();
+        await transaction1.CommitAsync();
+
+        var transaction2 = await _sut.BeginTransactionAsync();
+        _context.AddRange(data2);
+        await _context.SaveChangesAsync();
+        await transaction2.RollbackAsync();
+
+        // Assert
+        _context.Test.Should().BeEquivalentTo(data1);
     }
 
 
