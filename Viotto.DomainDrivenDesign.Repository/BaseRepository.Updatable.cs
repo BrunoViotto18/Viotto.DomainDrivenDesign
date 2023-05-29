f176f1a74ca83aa10215d83360a6601ab90f2165
Santos Bruno (CtP/ETS) <brunoviottosantos@gmail.com>
2023-05-29 13:19:26 -0300

    Adding tests for the update methods base implementation of the BaseRepository abstract class. #11

--- Viotto.DomainDrivenDesign.Repository/BaseRepository.Updatable.cs
+++ Viotto.DomainDrivenDesign.Repository/BaseRepository.Updatable.cs
@@ -38,6 +38,11 @@ public abstract partial class BaseRepository<TContext, TModel, TId>
         if (!await BaseUpdate(model))
             return;
 
+        if (!GetAllNoTracking().Any(x => x.Id.Equals(model.Id)))
+        {
+            throw new InvalidOperationException();
+        }
+
         Context.Update(model);
         await Context.SaveChangesAsync();
     }
@@ -53,6 +58,13 @@ public abstract partial class BaseRepository<TContext, TModel, TId>
     {
         if (!await BaseUpdateRange(models))
             return;
+
+        var ids = models.Select(x => x.Id);
+
+        if (GetAllNoTracking().Where(x => ids.Contains(x.Id)).Count() != ids.Count())
+        {
+            throw new InvalidOperationException();
+        }
 
         Context.UpdateRange(models);
         await Context.SaveChangesAsync();
