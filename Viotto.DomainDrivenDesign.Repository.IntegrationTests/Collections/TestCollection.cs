37ccea0398e76b0ab8fe3ef591bb29169710ba1d
Bruno Viotto <brunoviottosantos@gmail.com>
2023-12-05 20:48:13 -0300

    Deleting TestCollections

--- Viotto.DomainDrivenDesign.Repository.IntegrationTests/Collections/TestCollection.cs
+++
@@ -1,9 +0,0 @@
-﻿using Testcontainers.MsSql;
-
-namespace Viotto.DomainDrivenDesign.Repository.IntegrationTests;
-
-
-[CollectionDefinition(nameof(TestCollection))]
-public class TestCollection : ICollectionFixture<TestSetup<MsSqlContainer>>
-{
-}
