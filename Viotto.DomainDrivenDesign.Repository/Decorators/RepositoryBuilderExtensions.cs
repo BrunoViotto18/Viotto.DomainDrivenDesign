531cd89252ed975eb8555065df074bc755600cca
Bruno Viotto <brunoviottosantos@gmail.com>
2023-12-05 20:47:02 -0300

    Adding the SoftDeleteDecorator, which makes it easy to implement simple softDelete logic in repositories #13

---
+++ Viotto.DomainDrivenDesign.Repository/Decorators/RepositoryBuilderExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq.Expressions;
+using Viotto.DomainDrivenDesign.Model;
+
+namespace Viotto.DomainDrivenDesign.Repository.Decorators;
+
+public static class RepositoryBuilderExtensions
+{
+    public static RepositoryBuilder<TModel, TId> AddSoftDelete<TModel, TId>(
+        this RepositoryBuilder<TModel, TId> builder,
+        Expression<Func<TModel, object?>> deleteProperty,
+        Expression<Func<TModel, bool>> isDeletedPrdicate,
+        Action<TModel> deleteAction)
+        where TModel : class, IEntity<TId>, new()
+    {
+        return builder.AddDecorator<SoftDeleteDecorator<TModel, TId>>(deleteProperty, isDeletedPrdicate, deleteAction);
+    }
+}
---
+++ Viotto.DomainDrivenDesign.Repository/Decorators/SoftDeleteDecorator.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using Viotto.DomainDrivenDesign.Model;
+
+namespace Viotto.DomainDrivenDesign.Repository.Decorators;
+
+public class SoftDeleteDecorator<TModel, TId> : RepositoryDecorator<TModel, TId>
+    where TModel : class, IEntity<TId>, new()
+{
+    private readonly Expression<Func<TModel, object?>> _deleteProperty;
+    private readonly Expression<Func<TModel, bool>> _isNotDeleted;
+    private readonly Action<TModel> _delete;
+
+    public SoftDeleteDecorator(
+        DbContext context,
+        IRepository<TModel, TId> repository,
+        Expression<Func<TModel, object?>> deletePropeprty,
+        Expression<Func<TModel, bool>> isDeletedPredicate,
+        Action<TModel> deleteAction)
+        : base(context, repository)
+    {
+        _deleteProperty = deletePropeprty;
+        var isNotDeletedExpression = Expression.Not(isDeletedPredicate.Body).Reduce();
+        _isNotDeleted = Expression.Lambda<Func<TModel, bool>>(isNotDeletedExpression, isDeletedPredicate.Parameters.First());
+        _delete = deleteAction;
+    }
+
+    public override IQueryable<TModel> GetAll()
+    {
+        return base.GetAll().Where(_isNotDeleted);
+    }
+
+    public override IQueryable<TModel> GetAllNoTracking()
+    {
+        return base.GetAllNoTracking().Where(_isNotDeleted);
+    }
+
+    public override IQueryable<TModel> GetById(TId id)
+    {
+        return base.GetById(id).Where(_isNotDeleted);
+    }
+
+    public override IQueryable<TModel> GetByIdNoTracking(TId id)
+    {
+        return base.GetByIdNoTracking(id).Where(_isNotDeleted);
+    }
+
+    public override void Remove(TModel model)
+    {
+        Context.Attach(model);
+        _delete(model);
+        Context.Entry(model).Property(_deleteProperty).IsModified = true;
+    }
+
+    public override void BulkRemove(IEnumerable<TModel> models)
+    {
+        Context.AttachRange(models);
+        foreach (var model in models)
+        {
+            _delete(model);
+            Context.Entry(model).Property(_deleteProperty).IsModified = true;
+        }
+    }
+
+    public override void RemoveById(TId id)
+    {
+        var model = new TModel()
+        {
+            Id = id
+        };
+        Context.Attach(model);
+        _delete(model);
+        Context.Entry(model).Property(_deleteProperty).IsModified = true;
+    }
+
+    public override void BulkRemoveById(IEnumerable<TId> ids)
+    {
+        var models = ids
+            .Select(id => new TModel
+            {
+                Id = id
+            })
+            .ToArray();
+        Context.AttachRange(models);
+        foreach (var model in models)
+        {
+            _delete(model);
+            Context.Entry(model).Property(_deleteProperty).IsModified = true;
+        }
+    }
+}
